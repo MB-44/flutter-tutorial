a51b88864933abf8a5589a18990e4d6a2b710c09
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-11-28 23:21:55 +0530

    created | 11/28 | 11.21pm

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({Key? key}) : super(key: key);
+  @override
+  // ignore: library_private_types_in_public_api
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter-tutorial",
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello, Flutter!"),
+      ),
+      body: const Center(
+        child: Text("MB-44"),
+      ),
+    );
+  }
+}
