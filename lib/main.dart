062d2f55ea593ad93bc0f356ffd820d63b018a24
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-12-01 13:07:49 +0530

    added text input field

--- lib/main.dart
+++ lib/main.dart
@@ -4,39 +4,78 @@ void main() {
   runApp(const MainApp());
 }
 
+// main class
 class MainApp extends StatefulWidget {
   const MainApp({Key? key}) : super(key: key);
   @override
-  // ignore: library_private_types_in_public_api
-  _MainAppState createState() => _MainAppState();
+  State<MainApp> createState() => _MainAppState();
 }
 
 class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter-tutorial",
+      title: "Flutter-Tutorial",
       theme: ThemeData(
-        primarySwatch: Colors.orange,
+        primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
+// to display home page
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Hello, Flutter!"),
+        appBar: AppBar(
+          title: const Text("Hello, Flutter!"),
+        ),
+        body: const TestInputWidget());
+  }
+}
+
+// This is for get the input
+class TestInputWidget extends StatefulWidget {
+  const TestInputWidget({Key? key}) : super(key: key);
+
+  @override
+  State<TestInputWidget> createState() => _TestInputWidgetState();
+}
+
+class _TestInputWidgetState extends State<TestInputWidget> {
+  final controller = TextEditingController();
+  String text = "";
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  void changeText(text) {
+    if (text == "Hello World!") {
+      controller.clear();
+      text = "";
+    }
+    setState(() {
+      this.text = text;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      TextField(
+        controller: controller,
+        decoration: const InputDecoration(
+            prefixIcon: Icon(Icons.message), labelText: "Type a message"),
+        onChanged: (text) => changeText(text),
       ),
-      body: const Center(
-        child: Text("MB-44"),
-      ),
-    );
+      Text(text)
+    ]);
   }
 }
